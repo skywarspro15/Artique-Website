049ce49977f64841c205e6bd10d4e7370606b164
skwyarspro15 <potatobg648@gmail.com>
2023-12-16 14:51:27 +0800

    Moved Firebase analytics of Character Chat to main file

--- scripts/ai.js
+++ scripts/ai.js
@@ -1,5 +1,24 @@
 // Character Chat frontend
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import {
+  getAnalytics,
+  logEvent,
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC6qAWVMtfFpmuhr2GY0rrjN2LYMbmBoJo",
+  authDomain: "character-interaction-engine.firebaseapp.com",
+  projectId: "character-interaction-engine",
+  storageBucket: "character-interaction-engine.appspot.com",
+  messagingSenderId: "758446330757",
+  appId: "1:758446330757:web:403990dd02e63b251eaaec",
+  measurementId: "G-D7ZXR0P213",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const userQuestions = [
   "How's everyone doing today?",
   "Miku, what's your favorite thing about music?",
@@ -253,7 +272,7 @@ function destroyTypingIndicator() {
   }
 }
 
-function sendMessage() {
+async function sendMessage() {
   if (socket.connected) {
     socket.emit("userMessage", {
       text: msgInput.value,
@@ -261,6 +280,7 @@ function sendMessage() {
       characters: characters,
     });
     createMessage("You", msgInput.value, true);
+    logEvent(analytics, "send_message");
     msgInput.value = "";
   }
 }
